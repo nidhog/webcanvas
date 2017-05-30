38ba68a2642bb01be52e9d83f366907963c7e848
Ismail Elouafiq <ismail.elouafiq@telecom-bretagne.eu>
2017-05-30 20:59:17 +0200

    add contributions admin

--- epidemicanvas/admin.py
+++ epidemicanvas/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Session, Artist
+from .models import Session, Artist, Contributions
 
 admin.site.register(Session)
 admin.site.register(Artist)
+admin.site.register(Contributions)
